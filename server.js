cac84755e3f4a15e03c2480fa8d0c8af9bab303a
brettmadrid <brettmadrid9@gmail.com>
2019-03-27 00:22:14 -0700

    set up restricted middleware validation functioon to restrict request for getting data unless the user is logged in and validated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const db = require('./users/users-model');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const db = require("./users/users-model");
 
-const Users = require('./users/users-model.js');
+const Users = require("./users/users-model.js");
 
 const server = express();
 
@@ -15,11 +15,33 @@ server.use(cors());
 //server.use('/api/register', registerRouter)
 //server.use('/api/login', loginRouter)
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+// middleware function to validate a user
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid credentials!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Houston, we have a problem!" });
+      });
+  } else {
+    res.status(400).json({ message: "Credentials not provided!" });
+  }
+}
+
+server.post("/api/register", (req, res) => {
   const user = req.body;
 
   // hash the user's password
@@ -30,32 +52,32 @@ server.post('/api/register', (req, res) => {
 
   // using the users-model.js helper file, add new user to the database
   Users.add(user)
-  .then(saved => {
-    res.status(201).json(saved)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
-})
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
   Users.findBy({ username })
-  .first()
-  .then(user => {
-    if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({ message: `Welcome ${user.username}!` });
-    } else {
-      res.status(401).json({ message: "You shall not pass!"});
-    }
-  })
-  .catch(error => {
-    res.status(500).json(error);
-  })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-server.get('/api/users', (req, res) => {
+server.get("/api/users", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
